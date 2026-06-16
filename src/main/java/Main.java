ff248adb63045c0ea5c238eb04c46e337d7c1159
Fajan Sohaill <godfajan@gmail.com>
2026-06-17 01:53:53 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
+        Scanner sc = new Scanner(System.in);
          while (true) {
             System.out.print("$ ");
 
